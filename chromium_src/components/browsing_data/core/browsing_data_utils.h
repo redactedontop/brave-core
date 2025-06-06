6c64e660e4cc3323b0e6982daf003d29d92aa79d
Emerick Rogul <erogul@brave.com>
2025-06-06 15:44:19 -0400

    fixup! [cr138] Fix `BrowsingDataType` leo override

---
+++ chromium_src/components/browsing_data/core/browsing_data_utils.h
@@ -0,0 +1,12 @@
+/* Copyright (c) 2025 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// LINT.IfChange(BrowsingDataType)
+#define MAX_VALUE BRAVE_AI_CHAT = 9, MAX_VALUE = BRAVE_AI_CHAT, UNUSED
+// LINT.ThenChange(/chrome/browser/resources/settings/clear_browsing_data_dialog/clear_browsing_data_browser_proxy.ts:BrowsingDataType)
+
+#include "src/components/browsing_data/core/browsing_data_utils.h"
+
+#undef MAX_VALUE
